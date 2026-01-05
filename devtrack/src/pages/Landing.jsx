480406c2214dde09844aee042eb6f43e47c04b97
Adedamola <aderogbaadedamola8@gmail.com>
2026-01-05 19:01:41 +0100

    initial commit

---
+++ devtrack/src/pages/Landing.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+      <h1 className='text-xl text-slate-900 underline'>
+       Hello World
+      </h1>
+  )
+}
+
+export default App
